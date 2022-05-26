950ce98bad416cf74239235ea09dcd382d4f5246
Katie Garcia <66224990+kaygar86@users.noreply.github.com>
2022-05-26 13:49:52 -0700

    attempting challenge del 1

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Challenge_starter_code.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Queries/Employee_Challenge_starter_code.sql
@@ -1,10 +1,10 @@
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+SELECT DISTINCT ON (employees) titles,
+emp_no,
+first_name,
+last_name
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+INTO retirement_titles
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no, _____ DESC;
